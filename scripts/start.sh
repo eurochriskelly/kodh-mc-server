e0519f6cf0ffa094bd47100a61299cb145006313
Chris Kelly <eurochriskelly@gmail.com>
2024-05-20 00:00:01 +0200

    Support geyser for bedrock

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 confDir=minecraft/plugins/Geyser-Spigot
 mkdir -p $confDir 
-cp config/geyser.yml $confDir/config.yml
+# cp config/geyser.yml $confDir/config.yml
 
 if [ ! -f "$confDir/../Geyser-Spigot.jar" ];then
   echo "Error: geyser missing. Not going to start minecraft if everyone can't join :("
